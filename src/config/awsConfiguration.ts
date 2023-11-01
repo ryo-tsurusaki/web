654889b15e0a6ae8b7fdabf89502d7ecaec84343
r-tsurusaki <ryo-tsurusaki@ginga.info>
2023-11-01 21:12:30 +0900

    Cognitoの認証を仮実装

---
+++ src/config/awsConfiguration.ts
@@ -0,0 +1,7 @@
+export const awsConfiguration = {
+    region: process.env.REACT_APP_COGNITO_REGION,
+    IdentityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+    UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID || '',
+    ClientId: process.env.REACT_APP_COGNITO_CLIENT_ID || '',
+  }
+  
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,79 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 import { Button } from "@mui/material";
+import TextField from '@mui/material/TextField';
 import { Link } from "react-router-dom";
-
+import { CognitoUserPool, CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js"
+import { awsConfiguration } from '../config/awsConfiguration'
+  
+const userPool = new CognitoUserPool({
+  UserPoolId: awsConfiguration.UserPoolId,
+  ClientId: awsConfiguration.ClientId,
+})
+  
 const Login: FC = () => {
+
+  const [email, setEmail] = React.useState<string>('')
+  const [password, setPassword] = React.useState<string>('')
+  const changedEmailHaldler = (e: any) => setEmail(e.target.value)
+  const changedPasswordHandler = (e: any) => setPassword(e.target.value)
+
+  const signIn = () => {
+    const authenticationDetails = new AuthenticationDetails({
+      Username : email,
+      Password : password
+    })
+    const cognitoUser = new CognitoUser({
+      Username: email,
+      Pool: userPool
+    })
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        console.log('result: ' + result)
+        const accessToken = result.getAccessToken().getJwtToken()
+        console.log('AccessToken: ' + accessToken)
+        setEmail('')
+        setPassword('')
+        console.log("OK, signIn");
+      },
+      onFailure: (err) => {
+        alert('NG, Login please check email, password');
+        console.log("NG, signIn:onFailure");
+        console.error(err)
+      }
+    })
+  }
+
+  const checkLogin = () => {
+    const cognitoUser = userPool.getCurrentUser()
+    if (cognitoUser) {
+      console.log('OK, sign in')
+      console.log(cognitoUser)
+    } else {
+      console.log('not, signing in')
+    }
+  }
 
     return (
         <div>
             LOGIN
-            <Button variant="outlined" size="large" component={Link} to="/login">
-                MENU
-            </Button>
-        
+            <TextField
+              required
+              id="outlined-required"
+              label="Required"
+              defaultValue="Hello World"
+              onChange={changedEmailHaldler}
+            />
+            <TextField
+              id="outlined-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              onChange={changedPasswordHandler}
+            />
+            <Button variant="outlined" size="large"  onClick={signIn}>MENU</Button>
+            <br />
+            <Button variant="outlined" size="large"  onClick={checkLogin}>checkLogin</Button>
         </div>
     );
 }
