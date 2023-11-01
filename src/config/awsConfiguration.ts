f99aec834d96f6ddc621d344bbc5cc2374ca351a
r-tsurusaki <ryo-tsurusaki@ginga.info>
2023-11-01 18:40:01 +0900

    u

--- src/config/awsConfiguration.ts
+++ src/config/awsConfiguration.ts
@@ -1,6 +1,6 @@
 export const awsConfiguration = {
     region: 'ap-northeast-1',
-    IdentityPoolId: 'ap-northeast-1:eabf39dc-220f-40a5-8555-6530d601f35e',
-    UserPoolId: 'ap-northeast-1_gHeKD9NB2',
-    ClientId: '4b11ahieij5hfq6h7d890e7ld4',
+    IdentityPoolId: '',
+    UserPoolId: '',
+    ClientId: '',
 }
