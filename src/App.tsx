ae73d54ebc3b183195f41e6b109084e145ddd9fd
r-tsurusaki <ryo-tsurusaki@ginga.info>
2023-11-05 23:54:52 +0900

    DrawerをComponent化

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { FC, useState, useEffect } from 'react';
-import { Link, Routes, Route, useLocation } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import MuiDrawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import HomeIcon from '@mui/icons-material/Home';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import { RootState } from './store';
+import CommonDrawer from './component/CommonDrawer';
 import ErrorBoundary from './pages/errorBoundary';
 import Index from './pages/home';
 import Login from './pages/login';
 
-const drawerWidth = 240;
-
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}));
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
-
 const App: FC = () => {
   const pathName = useLocation().pathname;
-  const theme = useTheme();
-  const [open, setOpen] = useState(false);
   const userState = useSelector((state: RootState) => state.user)
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-    console.log(userState)
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const title = () => {
-    switch(pathName) {
-      case '/':
-        return 'HOME';
-      case '/login':
-        return 'ログイン';
-      default:
-        return 'undefined';
-    }
-  };
-
-  let isLogin = false;
-  
 
   useEffect(
     () => {
-
-      console.log(userState)
-
+      let isLogin = false;
       if (userState.name !== undefined) {
         isLogin = true;
       }
@@ -143,55 +30,8 @@ const App: FC = () => {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            {title()}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <List>
-          <ListItem key='Home' disablePadding sx={{ display: 'block' }}>
-            <ListItemButton component={Link} to="/" sx={{minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5,}}>
-              <ListItemIcon sx={{minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center'}}>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText primary='Home' sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem key='Login' disablePadding sx={{ display: 'block' }}>
-            <ListItemButton component={Link} to="/login" sx={{minHeight: 48, justifyContent: open ? 'initial' : 'center', px: 2.5,}}>
-              <ListItemIcon sx={{minWidth: 0, mr: open ? 3 : 'auto', justifyContent: 'center'}}>
-                <AccountCircle />
-              </ListItemIcon>
-              <ListItemText primary='Login' sx={{ opacity: open ? 1 : 0 }} />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
+      <CommonDrawer />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
         <ErrorBoundary>
           <Routes>
             <Route path="/" element={<Index />} />
