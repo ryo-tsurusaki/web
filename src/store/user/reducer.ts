db6c9f59d2de292ffcfce1812d67071a3adba13f
r-tsurusaki <ryo-tsurusaki@ginga.info>
2023-11-05 23:34:52 +0900

    lint fix

--- src/store/user/reducer.ts
+++ src/store/user/reducer.ts
@@ -1,16 +1,19 @@
-import { ActionTypes } from "../actionTypes";
-import { User, UserActionTypes } from "./types";
+import { ActionTypes } from '../actionTypes';
+import { User, UserActionTypes } from './types';
 
-const initialState : User = {
-    name: undefined,
-}
+const initialState: User = {
+  name: undefined,
+};
 
-// eslint-disable-next-line default-param-last
-export const UserReducer = (state = initialState, action: UserActionTypes) : User => {
-    switch (action.type) {
-        case ActionTypes.set:
-            return { ...state, name: action.value }
-        default:
-            return state
-    }
-}
+export const UserReducer = (
+  // eslint-disable-next-line default-param-last
+  state = initialState,
+  action: UserActionTypes,
+): User => {
+  switch (action.type) {
+    case ActionTypes.set:
+      return { ...state, name: action.value };
+    default:
+      return state;
+  }
+};
